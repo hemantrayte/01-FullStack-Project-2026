6a0be6fdacb7a8c89e184c4abdc83b8b99c9f55a
hemant <hemantrayte25@gmail.com>
2026-01-19 10:43:14 +0530

    create task controller

---
+++ Backend/src/controllers/task.controller.js
@@ -0,0 +1,38 @@
+import { Task } from "../models/task.model.js";
+import { Board } from "../models/board.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+const createTask = asyncHandler(async (req, res) => {
+  const { title, description, boardId, columnId, priority, dueDate } = req.body;
+
+  if (!title || !boardId || !columnId) {
+    throw new ApiError(400, "Title, boardId and columnId are required");
+  }
+
+  // Check board exists
+  const board = await Board.findById(boardId);
+  if (!board) {
+    throw new ApiError(404, "Board not found");
+  }
+
+  const task = await Task.create({
+    title,
+    description,
+    board: boardId,
+    column: columnId,
+    createdBy: req.user._id,
+    priority,
+    dueDate,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, task, "Task created successfully"));
+});
+
+
+export {
+  createTask,
+}
