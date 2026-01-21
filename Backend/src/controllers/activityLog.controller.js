ad4ea57ce04a6b97f4dff454fc39371722e64191
hemant <hemantrayte25@gmail.com>
2026-01-21 19:13:05 +0530

    intial all controllers of activityLog

---
+++ Backend/src/controllers/activityLog.controller.js
@@ -0,0 +1,48 @@
+import { ActivityLog } from "../models/activityLog.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+
+const createActivityLog = asyncHandler(async(req , res) => {
+
+})
+
+const getAllActivityLogs = asyncHandler(async(req , res) => {
+  
+})
+
+const getWorkspaceLogs = asyncHandler(async(req , res) => {
+  
+})
+
+const getBoardLogs = asyncHandler(async(req , res) => {
+  
+})
+
+const getTaskLogs = asyncHandler(async(req , res) => {
+  
+})
+
+const getUserLogs = asyncHandler(async(req , res) => {
+  
+})
+
+const deleteActivityLog = asyncHandler(async(req , res) => {
+  
+})
+
+const clearEntityLogs = asyncHandler(async(req , res) => {
+  
+})
+
+export {
+  createActivityLog,
+  getAllActivityLogs,
+  getWorkspaceLogs,
+  getBoardLogs,
+  getUserLogs,
+  getTaskLogs,
+  clearEntityLogs,
+  deleteActivityLog
+}
