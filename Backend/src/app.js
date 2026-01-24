636d6bef21ad0ef01c97c7195d7ea6c393dea02b
hemant <hemantrayte25@gmail.com>
2026-01-24 21:47:28 +0530

    adding comments to understnd

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//import routes
 import userRoutes from "./routes/user.routes.js";
 import boardRoutes from "./routes/board.routes.js";
 
-
+//routes
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/boards", boardRoutes);
 
