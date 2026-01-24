cb2492ae444ec2059b6e6b4d16dbf43e972fc2f8
hemant <hemantrayte25@gmail.com>
2026-01-24 21:44:40 +0530

    userroute in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+import userRoutes from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRoutes);
 
 
 export default app
