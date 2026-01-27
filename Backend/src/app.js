28c79ef171feb949ac82172d232dfe559b6413cd
hemant <hemantrayte25@gmail.com>
2026-01-27 09:12:59 +0530

    define workspace router in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,11 +9,12 @@ app.use(express.json());
 //import routes
 import userRoutes from "./routes/user.routes.js";
 import boardRoutes from "./routes/board.routes.js";
+import workspaceRoutes from "./routes/workspace.routes.js";
 
 //routes
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/boards", boardRoutes);
-
+app.use("/api/v1/workspaces", workspaceRoutes);
 
 
 export default app
