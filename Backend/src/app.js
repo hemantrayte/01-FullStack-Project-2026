8d5fb23a6e6c31cb6c0def0899dc291f3a22941a
hemant <hemantrayte25@gmail.com>
2026-01-27 09:16:22 +0530

    define task router

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,11 +10,12 @@ app.use(express.json());
 import userRoutes from "./routes/user.routes.js";
 import boardRoutes from "./routes/board.routes.js";
 import workspaceRoutes from "./routes/workspace.routes.js";
+import taskRoutes from "./routes/task.routes.js";
 
 //routes
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/boards", boardRoutes);
 app.use("/api/v1/workspaces", workspaceRoutes);
-
+app.use("/api/v1/tasks", taskRoutes)
 
 export default app
